74a0014bd387f57da6c06ed7f66550b8783037a3
Ashley Rogers <130684731+arogersrenee@users.noreply.github.com>
2023-08-10 16:34:45 -0500

    js mobile share button working

---
+++ script.js
@@ -0,0 +1,20 @@
+const desktopSize = window.matchMedia("(min-width: 1440px)");
+const mobileSize = window.matchMedia("(max-width: 1439px)");
+
+const shareButton = document.querySelector('#share-button'),
+      attributeSection = document.querySelector('.article-attributes'),
+      shareSection = document.querySelector('.share-section'),
+      footer = document.querySelector('footer');
+      
+shareButton.addEventListener('click', showSocial);
+
+function showSocial() {
+    if(mobileSize.matches){
+        shareButton.firstElementChild.classList.toggle('share-icon-share');
+        shareButton.classList.toggle('button-share');
+        footer.classList.toggle('footer-share');
+        shareSection.classList.toggle('hidden');
+        attributeSection.classList.toggle('hidden')
+    }
+
+}
