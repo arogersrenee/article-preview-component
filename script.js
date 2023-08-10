18809617ae089b70fd99a7968b2935a3bae6d6da
Ashley Rogers <130684731+arogersrenee@users.noreply.github.com>
2023-08-10 17:10:34 -0500

    wrapped function in modlular pattern

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-const desktopSize = window.matchMedia("(min-width: 1440px)");
-const mobileSize = window.matchMedia("(max-width: 1439px)");
+const shareSocial = (() => {
+    const desktopSize = window.matchMedia("(min-width: 1440px)"),
+          mobileSize = window.matchMedia("(max-width: 1439px)"),
 
-const shareButton = document.querySelector('#share-button'),
-      attributeSection = document.querySelector('.article-attributes'),
-      shareSection = document.querySelector('.share-section'),
-      footer = document.querySelector('footer'),
-      shareTip = document.querySelector('.share-tip');
-      
-shareButton.addEventListener('click', showSocial);
+          shareButton = document.querySelector('#share-button'),
+          attributeSection = document.querySelector('.article-attributes'),
+          shareSection = document.querySelector('.share-section'),
+          footer = document.querySelector('footer'),
+          shareTip = document.querySelector('.share-tip');
+          
+    shareButton.addEventListener('click', showSocial);
 
-function showSocial() {
-    if(mobileSize.matches){
-        shareButton.firstElementChild.classList.toggle('share-icon-share');
-        shareButton.classList.toggle('button-share');
-        footer.classList.toggle('footer-share');
-        shareSection.classList.toggle('hidden');
-        attributeSection.classList.toggle('hidden');
-    } else {
-        shareTip.classList.toggle('share-tip-hide')
+    function showSocial() {
+        if(mobileSize.matches){
+            shareButton.firstElementChild.classList.toggle('share-icon-share');
+            shareButton.classList.toggle('button-share');
+            footer.classList.toggle('footer-share');
+            shareSection.classList.toggle('hidden');
+            attributeSection.classList.toggle('hidden');
+        } else {
+            shareTip.classList.toggle('share-tip-hide')
+        }
     }
 
-}
+})()
+
